c63684569f733edb4b6523cd799825614c9b5015
kotoyuuko <kokoro@core.moe>
2019-03-29 22:53:26 +0800

    basic layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://core.moe" target="_blank">kotoyuuko</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+    <p class="float-right"><a href="mailto:kotoyuuko@gmail.com">联系我</a></p>
+  </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+  <div class="container">
+    <a class="navbar-brand " href="{{ url('/') }}">
+      USTBClass
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+      </ul>
+      
+      <ul class="navbar-nav navbar-right">
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
