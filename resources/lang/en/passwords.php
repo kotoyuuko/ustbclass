228075643ab4aca965001234f205b9ac00cf2e4d
kotoyuuko <kokoro@core.moe>
2019-04-04 23:55:01 +0800

    translation

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -1,10 +1,9 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
-    | Password Reset Language Lines
+    | Password Reminder Language Lines
     |--------------------------------------------------------------------------
     |
     | The following language lines are the default lines which match reasons
@@ -13,10 +12,9 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
-
+    'password' => '密码至少是六位字符并且应与确认密码匹配。',
+    'reset'    => '密码重置成功！',
+    'sent'     => '密码重置邮件已发送！',
+    'token'    => '密码重置令牌无效。',
+    'user'     => '找不到该邮箱对应的用户。',
 ];
