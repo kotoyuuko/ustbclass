461810680e832f201ea66f349606d6c1fbf4ade3
kotoyuuko <kokoro@core.moe>
2019-04-06 00:22:13 +0800

    fix schedule jobs frequency

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new \App\Jobs\UpdateCourses)->everyMinute();
-        //$schedule->job(new \App\Jobs\UpdateCourses)->weekends()->everyFiveMinutes();
+        $schedule->job(new \App\Jobs\UpdateCourses)->weekends()->everyFiveMinutes();
         $schedule->job(new \App\Jobs\UpdateCurrentWeek)->weeklyOn(1, '8:00');
     }
 
